7049c5949b1a4e8c5435f90c9499c1c60844dd83
Binamin-hussein100 <binamin.hassan@student.moringaschool.com>
2021-08-16 19:55:45 +0300

    added the run statement

---
+++ manage.py
@@ -0,0 +1,23 @@
+from app import create_app,db
+from flask_script import Manager,Server
+from app.models import User
+from flask_migrate import Migrate
+
+# creating an app instance
+app = create_app('development')
+
+
+
+manager = Manager(app)
+manager.add_command('server',Server)
+
+migrate = Migrate(app,db)
+manager.add_command('db')
+
+
+@manager.shell
+def make_shell_context():
+    return dict(app = app, db = db , User = User)
+
+if __name__ == '__main__':
+    manager.run()
