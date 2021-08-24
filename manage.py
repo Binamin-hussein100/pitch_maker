5266b10d9c8ea7c18910c7dc3d86a7611973341f
Binamin-hussein100 <binamin.hassan@student.moringaschool.com>
2021-08-24 14:51:37 +0300

    used cli

--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 from app import create_app,db
 from flask_script import Manager,Server
-from app.models import User
 from flask_migrate import Migrate
+from app.models import User
 
 # creating an app instance
 app = create_app('development')
@@ -15,9 +15,7 @@ migrate = Migrate(app,db)
 manager.add_command('db')
 
 
-@manager.shell
+@app.shell_context_processor
 def make_shell_context():
     return dict(app = app, db = db , User = User)
 
-if __name__ == '__main__':
-    manager.run()
